08f73ec3d16bf71dacdd5c106cfbef8def3fab58
Igor <ihor.zhelezniak@mths.ca>
2021-11-10 17:19:37 +0000

    updated

--- program.py
+++ program.py
@@ -8,6 +8,8 @@ import math
 
 
 def quadratic(a, b, c, add):
+    # this function calculate x1 and x2
+
     x_1 = -b / (2 * a)
     x_2 = math.sqrt(b ** 2 - 4 * a * c) / (2 * a)
     if add == True:
@@ -27,19 +29,25 @@ def main():
 
     # process
     try:
-        a_1 = int(integer1)
-        b_1 = int(integer2)
-        c_1 = int(integer3)
+        a_1 = float(integer1)
+        b_1 = float(integer2)
+        c_1 = float(integer3)
         d = b_1 ** 2 - 4 * a_1 * c_1
         if d < 0:
             print("There is no answer")
         elif d == 0:
             answer = quadratic(a=a_1, b=b_1, c=c_1, add=True)
-            print("The answer is {0}".format(answer))
+            print("If the quadratic equation looks like this:")
+            print("{0}x² + {1}x + {2}".format(a_1, b_1, c_1))
+            print("Discriminant = {0}".format(d))
+            print("x = {0}".format(answer))
         else:
             answer1 = quadratic(add=True, a=a_1, c=c_1, b=b_1)
             answer2 = quadratic(b=b_1, c=c_1, a=a_1, add=False)
-            print("The answers are {0} and {1}".format(answer1, answer2))
+            print("If the quadratic equation looks like this:")
+            print("{0}x² + {1}x + {2}".format(a_1, b_1, c_1))
+            print("Discriminant = {0}".format(d))
+            print("x = {0} or {1}".format(answer1, answer2))
     except Exception:
         print("Invalid input")
     finally:
